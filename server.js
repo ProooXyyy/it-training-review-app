daf6b6aa0f4596d768c134513674e374cfd60a37
ProooXyyy <bhosleyash18@gmail.com>
2026-05-08 13:48:12 +0530

    Add Vercel configuration

--- server.js
+++ server.js
@@ -49,3 +49,5 @@ app.post('/generate-review', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+module.exports = app;
